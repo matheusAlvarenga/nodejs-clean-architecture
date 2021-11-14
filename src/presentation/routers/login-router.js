ec2f730496f1f337cf6529c4879314bd651c7b51
matheusAlvarenga <matheus.alvarenga0302@gmail.com>
2021-11-14 18:10:01 -0300

    enxure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,11 +1,18 @@
 const HttpResponse = require('../helpers/http-response');
 
 module.exports = class LoginRouter {
+  constructor(authUseCase) {
+    this.authUseCase = authUseCase;
+  }
+
   route(httpRequest) {
     if (!httpRequest || !httpRequest.body) return HttpResponse.serverError();
 
     const { email, password } = httpRequest.body;
+
     if (!email) return HttpResponse.badRequest('email');
     if (!password) return HttpResponse.badRequest('password');
+
+    this.authUseCase.auth(email, password);
   }
 };
