7d10c4b801fe94628c4cd2c952538baf6f16b586
matheusAlvarenga <matheus.alvarenga0302@gmail.com>
2021-11-23 09:25:45 -0300

    add and configure jest-mongodb

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['**/src/**/*.js'],
   coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig'],
 };
