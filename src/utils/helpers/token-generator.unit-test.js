15909a79ddf1d68536edac24cc0922d0344ae655
matheusAlvarenga <matheus.alvarenga0302@gmail.com>
2021-11-23 07:45:34 -0300

    ensure TokenGenerator returns null if JWT returns null

---
+++ src/utils/helpers/token-generator.unit-test.js
@@ -0,0 +1,13 @@
+class TokenGenerator {
+  async generate() {
+    return null;
+  }
+}
+
+describe('Token Generator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator();
+    const token = await sut.generate('any_id');
+    expect(token).toBeNull();
+  });
+});
