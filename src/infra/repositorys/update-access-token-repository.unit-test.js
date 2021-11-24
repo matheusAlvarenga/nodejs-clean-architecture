3c23752022a127da072d0a90d98b0cc05ed67e31
matheusAlvarenga <matheus.alvarenga0302@gmail.com>
2021-11-24 07:43:29 -0300

    ensure UpdateAccessTokenRepository updates user with the given accessToken

---
+++ src/infra/repositorys/update-access-token-repository.unit-test.js
@@ -0,0 +1,46 @@
+const MongoHelper = require('../helpers/mongo-helper');
+
+let db;
+
+class UpdateAccessTokenRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+
+  async update(userId, accessToken) {
+    await this.userModel.updateOne({ _id: userId }, {
+      $set: {
+        accessToken,
+      },
+    });
+  }
+}
+
+describe('UpdateAccessToken Repository', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    await MongoHelper.connect(global.__MONGO_URI__);
+    db = MongoHelper.db;
+  });
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('should update the user with the given accessToken', async () => {
+    const userModel = db.collection('users');
+    const sut = new UpdateAccessTokenRepository(userModel);
+
+    const fakeUser = await userModel.insertOne({ email: 'valid_email@mail.com', password: 'any_hashed_password' });
+
+    await sut.update(fakeUser.insertedId, 'valid_token');
+
+    const updatedFakeUser = await userModel.findOne({ _id: fakeUser.insertedId });
+
+    expect(updatedFakeUser.accessToken).toBe('valid_token');
+  });
+});
